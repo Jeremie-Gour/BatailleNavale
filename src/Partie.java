b9b45e220a1a1c4d11c5141de6219ad79593a47d
fabreezy <46725018+fabreezy@users.noreply.github.com>
2021-04-26 18:30:54 -0400

    AJoute ordinateur

--- src/Partie.java
+++ src/Partie.java
@@ -4,24 +4,26 @@
 public class Partie {
     JoueurHumain joueur = new JoueurHumain();
     JoueurOrdinateur ordinateur = new JoueurOrdinateur();
-    Ocean oceanHumain = new Ocean();
-    Ocean oceanOrdinateur = new Ocean();
+    Ocean oceanFacile = new Ocean();
+    Ocean oceanIntermediaire = new Ocean();
+    Ordinateur facile = new Ordinateur(Difficulte.FACILE);
+    Ordinateur difficile = new Ordinateur(Difficulte.DIFFICILE);
 
 
     // En ce moment, deux ordis peuvent jouer l'un contre l'autre à Facile.
     // On s'occupera des saisies claviers plus tard, ça ne change presque rien à la conception.
     // ** Quand on affiche la grille de l'autre, il faudrait afficher de l'eau ('~') à la place des bateaux/bombes -- P-e faire 2 fonctions Partie.afficherOcean() **
     public void jouerPartie() {
-        boolean tirHumain = true;
-        boolean tirOrdinateur = true;
+        TypeCellule celluletireFacile;
+        TypeCellule celluletireIntermediaire;
         Difficulte difficulte = Difficulte.FACILE;
 
         // On peut placer les bombes, mais il faut regarder pour les vérifications et tirer une bombe ne fait pas encore passer un tour.
 //        oceanHumain.placerBombes();
 //        oceanOrdinateur.placerBombes();
-        oceanHumain.placerNaviresAleatoirement(TypeJoueur.ORDINATEUR);
-        oceanOrdinateur.placerNaviresAleatoirement(TypeJoueur.HUMAIN);
-        oceanOrdinateur.afficherOcean();
+        oceanFacile.placerNaviresAleatoirement(TypeJoueur.HUMAIN);
+        oceanIntermediaire.placerNaviresAleatoirement(TypeJoueur.ORDINATEUR);
+        //oceanOrdinateur.afficherOcean();
 
 
 
@@ -29,16 +31,18 @@ public class Partie {
         // Alterne de joueur en joueur jusqu'a temps qu'il y n'y a pas de gagnant
         // J'ai mis les tirs en boolean (un tir retourne true si il reste des bateaux à pew pew).
         // Va p-e falloir changer le retour des fonctions pour quand on shoot une bombe (typeCellule maybe)
-        while (tirHumain && tirOrdinateur ){
+        while (oceanIntermediaire.celluleDeNavireIntactesDispo() && oceanFacile.celluleDeNavireIntactesDispo() ){
+            Coordonnee coordonneeFacile = facile.prochaineAttaque();
+            celluletireFacile= oceanFacile.tirerOrdinateur(coordonneeFacile);
 
+            Coordonnee coordonneeIntermediaire = difficile.prochaineAttaque();
+            celluletireIntermediaire = oceanIntermediaire.tirerOrdinateur(coordonneeIntermediaire);
 
-            tirHumain = oceanOrdinateur.tirerOrdinateur(Difficulte.FACILE);
-            tirOrdinateur = oceanHumain.tirerOrdinateur(Difficulte.FACILE);
-
-
+            facile.recevoirResultat(celluletireFacile);
+            difficile.recevoirResultat(celluletireIntermediaire);
         }
 
-        Affichage.afficherGagnant(tirHumain, tirOrdinateur);
+        Affichage.afficherGagnant(oceanFacile.celluleDeNavireIntactesDispo(), oceanIntermediaire.celluleDeNavireIntactesDispo());
 
     }
 
