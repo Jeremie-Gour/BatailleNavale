5b8f27ac20488f6a23c1d7f3833042aab6003dc8
fabreezy <46725018+fabreezy@users.noreply.github.com>
2021-05-01 18:24:25 -0400

    Update Coordonee
    update coordonne

--- src/main/java/uqam/inf5153/batailleNavale/Coordonnee.java
+++ src/main/java/uqam/inf5153/batailleNavale/Coordonnee.java
@@ -1,5 +1,4 @@
 package uqam.inf5153.batailleNavale;
-
 /**
  * Cette classe représente une coordonnée sur la grille de jeu.
  */
@@ -12,6 +11,8 @@ public class Coordonnee {
         this.colonne = colonne;
     }
 
+
+
     public int getRangee() {
         return rangee;
     }
@@ -20,47 +21,39 @@ public class Coordonnee {
         return colonne;
     }
 
-    /**
-     * Décale le curseur de l'ordinateur de int distance vers le bas dans la grille de jeu.
-     * @param distance la distance du déplacement
-     */
-    public void deplacerEnBas(int distance) {
-        this.rangee -= distance;
+    public void deplacerEnBas() {
+        this.rangee--;
     }
 
-    /**
-     * Déplace le curseur de l'ordinateur d'une case en haut dans la grille de jeu.
-     */
-    public void deplacerEnHaut() {
-        this.rangee++;
+    public void deplacerEnBas(int pas) { this.rangee -= pas; }
+
+    public void deplacerEnHaut() { this.rangee++; }
+
+    public void deplacerEnHaut(int pas) { this.rangee+= pas ; }
+
+    public void deplacerAGauche() {
+        this.colonne--;
     }
 
-    /**
-     * Décale le curseur de l'ordinateur de int distance vers la gauche dans la grille de jeu.
-     * @param distance la distance du déplacement
-     */
-    public void deplacerAGauche(int distance) {
-        this.colonne -= distance;
-    }
+    public void deplacerAGauche(int pas) { this.colonne-= pas ; }
 
-    /**
-     * Déplace le curseur de l'ordinateur d'une case à droite dans la grille de jeu.
-     */
     public void deplacerADroite() {
         this.colonne++;
     }
 
-    /**
-     * Compare deux coordonnées entre elles.
-     * @param obj la coordonnée
-     * @return true si les coordonnées sont identiques, false sinon.
-     */
+    public void deplacerADroite(int pas) { this.colonne+= pas ; }
+
+    public void changerCoordonnes(int rangee, int colonne) {
+        this.rangee = rangee;
+        this.colonne = colonne;
+    }
+
     @Override
     public boolean equals(Object obj) {
-        if (obj == this) {
+        if(obj == this){
             return true;
         }
-        if (!(obj instanceof Coordonnee)) {
+        if(!(obj instanceof Coordonnee )){
             return false;
         }
         Coordonnee c = (Coordonnee) obj;
