cbbc8f48c354bda8d4dfe67bc69d7ccd7a53d73b
fabreezy <46725018+fabreezy@users.noreply.github.com>
2021-05-05 15:31:07 -0400

    ajout test ordinateur
    ajout de test ordinateur

--- src/test/java/uqam/inf5153/batailleNavale/OrdinateurTest.java
+++ src/test/java/uqam/inf5153/batailleNavale/OrdinateurTest.java
@@ -28,6 +28,7 @@ class OrdinateurTest {
     public void testerStrategieChercherAlignementIntermediaire1(){
         Ordinateur ordinateurIntermediaire = new Ordinateur(Difficulte.INTERMEDIAIRE);
         Coordonnee coordonneeSouhaité =  ordinateurIntermediaire.prochaineAttaque();
+
         coordonneeSouhaité.deplacerEnHaut(); //sa prochaine attaque devrait être la cellule en haut
         ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
         Coordonnee coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
@@ -41,7 +42,6 @@ class OrdinateurTest {
         Ordinateur ordinateurIntermediaire = new Ordinateur(Difficulte.INTERMEDIAIRE);
         Coordonnee coordonneeSouhaité =  ordinateurIntermediaire.prochaineAttaque();
         coordonneeSouhaité.deplacerEnBas();//au deuxième essaie, il devrait attaquer la cellule en bas
-
         ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
         Coordonnee coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
         ordinateurIntermediaire.recevoirResultat(TypeCellule.EAU);
@@ -66,6 +66,8 @@ class OrdinateurTest {
 
     }
 
+
+
     @Test
     public void testerStrategieSuivreAlignementIntermediaire1(){
         Ordinateur ordinateurIntermediaire = new Ordinateur(Difficulte.INTERMEDIAIRE);
@@ -87,6 +89,45 @@ class OrdinateurTest {
         coordonneeSouhaité.deplacerEnHaut(3); //sa prochaine attaque devrait être la cellule en haut
         ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
         Coordonnee coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+
+        assertEquals(coordonneeRecu,coordonneeSouhaité);
+    }
+
+    @Test
+    public void testerStrategieSuivreAlignementIntermediaire3(){
+        Ordinateur ordinateurIntermediaire = new Ordinateur(Difficulte.INTERMEDIAIRE);
+        Coordonnee coordonneeSouhaité =  ordinateurIntermediaire.prochaineAttaque();
+        coordonneeSouhaité.deplacerADroite(); //sa prochaine attaque devrait être la cellule en haut
+        coordonneeSouhaité.deplacerADroite();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
+        Coordonnee coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+
+        assertEquals(coordonneeRecu,coordonneeSouhaité);
+    }
+
+    @Test
+    public void testerStrategieSuivreAlignementIntermediaire4(){
+        Ordinateur ordinateurIntermediaire = new Ordinateur(Difficulte.INTERMEDIAIRE);
+        Coordonnee coordonneeSouhaité =  ordinateurIntermediaire.prochaineAttaque();
+        coordonneeSouhaité.deplacerADroite(); //sa prochaine attaque devrait être la cellule en haut
+        coordonneeSouhaité.deplacerADroite();
+        coordonneeSouhaité.deplacerADroite();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
+        Coordonnee coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurIntermediaire.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
         ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
         coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
         ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
@@ -153,15 +194,16 @@ class OrdinateurTest {
         assertTrue(coordonneeRecu.equals(coordonneeSouhaité));
 
     }
+
     @Test
     public void testerStrategieSuivreAlignementDifficile1(){
-        Ordinateur ordinateurIntermediaire = new Ordinateur(Difficulte.DIFFICILE);
-        Coordonnee coordonneeSouhaité =  ordinateurIntermediaire.prochaineAttaque();
+        Ordinateur ordinateurDifficile = new Ordinateur(Difficulte.DIFFICILE);
+        Coordonnee coordonneeSouhaité =  ordinateurDifficile.prochaineAttaque();
         coordonneeSouhaité.deplacerEnHaut(2); //sa prochaine attaque
-        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
-        Coordonnee coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
-        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
-        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        Coordonnee coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
 
         assertEquals(coordonneeRecu,coordonneeSouhaité);
 
@@ -169,19 +211,90 @@ class OrdinateurTest {
 
     @Test
     public void testerStrategieSuivreAlignementDifficile2(){
-        Ordinateur ordinateurIntermediaire = new Ordinateur(Difficulte.DIFFICILE);
-        Coordonnee coordonneeSouhaité =  ordinateurIntermediaire.prochaineAttaque();
+        Ordinateur ordinateurDifficile = new Ordinateur(Difficulte.DIFFICILE);
+        Coordonnee coordonneeSouhaité =  ordinateurDifficile.prochaineAttaque();
         coordonneeSouhaité.deplacerEnHaut(3); //sa prochaine attaque devrait être la cellule en haut
-        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
-        Coordonnee coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
-        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
-        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
-        ordinateurIntermediaire.recevoirResultat(TypeCellule.NAVIREINTACT);
-        coordonneeRecu = ordinateurIntermediaire.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        Coordonnee coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
 
         assertEquals(coordonneeRecu,coordonneeSouhaité);
     }
 
+    @Test
+    public void testerStrategieSuivreAlignementDifficile3(){
+        Ordinateur ordinateurDifficile = new Ordinateur(Difficulte.DIFFICILE);
+        Coordonnee coordonneeSouhaité =  ordinateurDifficile.prochaineAttaque();
+        coordonneeSouhaité.deplacerADroite(); //sa prochaine attaque devrait être la cellule en haut
+        coordonneeSouhaité.deplacerADroite();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        Coordonnee coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+
+        assertEquals(coordonneeRecu,coordonneeSouhaité);
+    }
+
+    @Test
+    public void testerStrategieSuivreAlignementDifficile4(){
+        Ordinateur ordinateurDifficile = new Ordinateur(Difficulte.DIFFICILE);
+        Coordonnee coordonneeSouhaité =  ordinateurDifficile.prochaineAttaque();
+        coordonneeSouhaité.deplacerADroite(); //sa prochaine attaque devrait être la cellule en haut
+        coordonneeSouhaité.deplacerADroite();
+        coordonneeSouhaité.deplacerADroite();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        Coordonnee coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+
+        assertEquals(coordonneeRecu,coordonneeSouhaité);
+    }
+
+    @Test
+    public void testerStrategieSuivreAutreBoutDifficile1(){
+        Ordinateur ordinateurDifficile = new Ordinateur(Difficulte.DIFFICILE);
+        Coordonnee coordonneeSouhaité =  ordinateurDifficile.prochaineAttaque();
+        coordonneeSouhaité.deplacerEnBas(1); //sa prochaine attaque devrait être la cellule en haut
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        Coordonnee coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+
+        assertEquals(coordonneeRecu,coordonneeSouhaité);
+    }
+    @Test
+    public void testerStrategieSuivreAutreBoutDifficile2(){
+        Ordinateur ordinateurDifficile = new Ordinateur(Difficulte.DIFFICILE);
+        Coordonnee coordonneeSouhaité =  ordinateurDifficile.prochaineAttaque();
+        coordonneeSouhaité.deplacerAGauche(1); //sa prochaine attaque devrait être la cellule en haut
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        Coordonnee coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.NAVIREINTACT);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+        ordinateurDifficile.recevoirResultat(TypeCellule.EAU);
+        coordonneeRecu = ordinateurDifficile.prochaineAttaque();
+
+        assertEquals(coordonneeRecu,coordonneeSouhaité);
+    }
 
 
 }
